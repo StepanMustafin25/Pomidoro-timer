9c38350277a6ee3a43f0bb9ad4af02d6e19b529a
StepanMustafin25 <stefanmyst@gmail.com>
2025-12-30 01:28:16 +0500

    Update footer.js

--- components/footer.js
+++ components/footer.js
@@ -4,7 +4,7 @@ class CustomFooter extends HTMLElement {
         this.innerHTML = `
             <footer class="bg-black bg-opacity-80 border-t-2 border-gray-700 mt-12 py-8">
                 <div class="container mx-auto px-4 text-center">
-                    <p class="text-gray-400 font-racing">© 2024 Pomodoro Grand Prix. Все права защищены.</p>
+                    <p class="text-gray-400 font-racing">© 2025 Pomodoro Grand Prix. Все права защищены.</p>
                     <p class="text-gray-500 text-sm mt-2">Техника помидоро для повышения продуктивности</p>
                 </div>
             </footer>
@@ -13,6 +13,7 @@ class CustomFooter extends HTMLElement {
 }
 
 customElements.define('custom-footer', CustomFooter);
+
 
 
 
